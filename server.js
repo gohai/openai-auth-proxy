69f17a71ffb0e69ae9fbee727c641a4d43673987
gohai <gottfried.haider@gmail.com>
2023-09-20 11:14:35 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const proxy = require('express-http-proxy');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+// Its strongly suggested to put the API key *not* here, but
+// instead create a new file named ".env" with the following
+// content:
+// OPENAI_API_KEY=...
+// That file should never be committed to GitHub or made public
+// in some other way.
+
+const api_key = process.env.OPENAI_API_KEY || '';
+if (!api_key) {
+  console.error('OPENAI_API_KEY is not set');
+}
+
+
+app.use('/', proxy('api.openai.com', {
+  https: true,
+  proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+    proxyReqOpts.headers['Authorization'] = 'Bearer ' + api_key;
+    return proxyReqOpts;
+  }
+}));
+
+app.use(cors({
+  origin: '*'
+}));
+
+app.listen(port, () => {
+  console.log('Server listening on port ' + port);
+});
